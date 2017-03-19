d1f128e86ff1848694d308ee56c80361e1bddbbe
2003cah <2003cah@gmail.com>
2017-03-19 18:37:53 -0400

    aaaaaa

--- code.rb
+++ code.rb
@@ -19,8 +19,8 @@ bot.server_create do |event|
   all_count = event.server.member_count
   members_count = event.server.online_members(include_idle: true, include_bots: false).count
   maths = all_count - members_count
-  maths_two = (maths / members_count)
-  percentage_count = maths_two * 100
+  maths_two = maths / members_count
+  percentage_count = maths_two / all_count * 100
   if percentage_count >= 50
     event.server.leave
     bot.send_message(281280895577489409, "Automatically left `#{event.server.name}` (ID: #{event.server.id}) due to high user:bot ratio (#{percentage_count}% bots)")
