fb59564b30e72c59a36475388e9ecdbc9dd9b1b2
2003cah <2003cah@gmail.com>
2017-03-25 14:57:30 -0400

    Well if I can't do that, let's try this

--- code.rb
+++ code.rb
@@ -19,9 +19,9 @@ bot.server_create do |event|
   all_count = event.server.member_count
   members_count = event.server.online_members(include_idle: true, include_bots: false).count
   maths = (members_count / all_count) * 100.0
-  if maths >= 50
+  if 50 >= maths
     event.server.leave
-    bot.send_message(281280895577489409, "Automatically left `#{event.server.name}` (ID: #{event.server.id}) due to high online user to bot ratio (#{maths}% bots)")
+    bot.send_message(281280895577489409, "Automatically left `#{event.server.name}` (ID: #{event.server.id}) due to high online user to bot ratio (#{maths}% online users)")
   else
     bot.send_message(281280895577489409, "CahBot Alpha just joined `#{event.server.name}` (ID: #{event.server.id}), owned by `#{event.server.owner.distinct}` (ID: #{event.server.owner.id}), the server count is now #{bot.servers.count}")
   end
