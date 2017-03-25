35134b48defb7dbe2f8aa568e8d26eb2d08b9e0e
2003cah <2003cah@gmail.com>
2017-03-25 16:13:34 -0400

    This is what I get for accidentally kicking Alpha

--- code.rb
+++ code.rb
@@ -15,17 +15,7 @@ bot.ready do |event|
   redo
 end
 
-bot.server_create do |event|
-  all_count = event.server.member_count
-  members_count = event.server.online_members(include_idle: true, include_bots: false).count
-  maths = (members_count / all_count) * 100.0
-  if maths <= 50
-    event.server.leave
-    bot.send_message(281280895577489409, "Automatically left `#{event.server.name}` (ID: #{event.server.id}) due to high online user to bot ratio (#{maths}% online users)")
-  else
-    bot.send_message(281280895577489409, "CahBot Alpha just joined `#{event.server.name}` (ID: #{event.server.id}), owned by `#{event.server.owner.distinct}` (ID: #{event.server.owner.id}), the server count is now #{bot.servers.count}")
-  end
-end
+
 
 bot.server_delete do |event|
   bot.send_message(281280895577489409, "CahBot Alpha just left `#{event.server.name}` (ID: #{event.server.id}), the server count is now #{bot.servers.count}")
