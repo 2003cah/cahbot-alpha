a18074e5119ae41b1405d9f8dd0bcb1f02305c0c
2003cah <2003cah@gmail.com>
2017-04-15 13:25:55 -0400

    Okay, I'll

--- code.rb
+++ code.rb
@@ -59,7 +59,7 @@ end
 bot.command(:report, help_available: false, min_args: 1) do |event, *args|
   bot.send_message(287050338144616449, "New Report, from `#{event.user.distinct}` (ID: #{event.user.id}), on server #{event.server.id} (ID: #{event.server.id}) \n \n#{event.message.content}")
   event.respond "Alright, do you want me to send an invite as well?"
-  bot.add_await(1, MessageEvent, from: event.user.id, content: 'Yes' || 'Sure' || 'Yeah') do |event, *args|
+  event.user.await(:aaaaa, content: 'Yes' || 'Sure' || 'Yeah') do |event, *args|
     "aaaaa testing things"
   end
 end
